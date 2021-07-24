b991f402ae463a32edde11dfee40f5864ba8eda5
Rafal <rafal.opial@coderslab.pl>
2021-07-24 11:46:00 +0200

    Optimize GloAppsHomePage to better initialize elements

--- src/test/java/pl/coderslab/pageobjectpatternonlteaw01/pageobject/GloappsHomePage.java
+++ src/test/java/pl/coderslab/pageobjectpatternonlteaw01/pageobject/GloappsHomePage.java
@@ -3,17 +3,25 @@ package pl.coderslab.pageobjectpatternonlteaw01.pageobject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 // this is our first Page Object
 public class GloappsHomePage {
-    private final WebDriver driver;
+    @FindBy(css = ".user_login")
+    private WebElement signInButton;
+    @FindBy(css = "div.shopping_cart a")
+    private WebElement shoppingCartLink;
 
     public GloappsHomePage(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void clickSignInButton() {
-        WebElement signInButton = driver.findElement(By.cssSelector(".user_login"));
         signInButton.click();
+    }
+
+    public void clickShoppingCart() {
+        shoppingCartLink.click();
     }
 }
